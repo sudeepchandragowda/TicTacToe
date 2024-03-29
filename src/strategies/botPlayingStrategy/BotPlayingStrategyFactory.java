3b6bc6ffb99c8c4a12e0889e131e52969cce9765
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-03-29 23:45:03 +0530

    Added BotPlayingStrategyFactory for object creation

---
+++ src/strategies/botPlayingStrategy/BotPlayingStrategyFactory.java
@@ -0,0 +1,13 @@
+package strategies.botPlayingStrategy;
+
+import models.BotDifficultyLevel;
+
+public class BotPlayingStrategyFactory {
+    public static BotPlayingStrategy getBotPlayingStrategyForDifficultyLevel(BotDifficultyLevel level) {
+        return switch (level) {
+            case EASY -> new RandomBotPlayingStrategy();
+            case MEDIUM -> new RandomBotPlayingStrategy();
+            case HARD -> new RandomBotPlayingStrategy();
+        };
+    }
+}
